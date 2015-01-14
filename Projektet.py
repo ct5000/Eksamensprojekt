f4c0005d2e520120d21f082372045f44d5c2f92a
Christian Thulstrup <ct.5000@hotmail.com>
2015-01-14 09:09:47 +0100

    Add structure

--- Projektet.py
+++ Projektet.py
@@ -1,8 +1,35 @@
 from Tkinter import *
 
 class MainApp():
-    __init__(self):
-        pass
+    def __init__(self, parent):
+        self.frame = Frame(parent)
+        self.frame.pack()
+        
+        self.button_fysik = Button(self.frame,text = "Fysik",
+                                   command = self.fysik)
+        self.button_fysik.pack()
+
+    def fysik(self):
+        fys = Fysik(self.frame)
+        
+
+class Fysik():
+    def __init__(self, parent):
+        self.top = Toplevel(parent)
+        msg = Message(self.top, text = "Test")
+        msg.pack()
+
+        button_kinematik = Button(self.top, text = "Kinematik",
+                                  command = self.kinematik)
+        button_kinematik.pack()
+
+    def kinematik(self):
+        print "Hello"
 
 
+
+root = Tk()
+app = MainApp(root)
+
+root.mainloop()
 
