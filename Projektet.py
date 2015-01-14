2faae7bf3c32e7b8bbdc0aa5a0bb0f38b71e70ef
Christian Thulstrup <ct.5000@hotmail.com>
2015-01-14 10:14:15 +0100

    Add selecting kin information

--- Projektet.py
+++ Projektet.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1252 -*-
 from Tkinter import *
 
 class MainApp():
@@ -24,9 +25,36 @@ class Fysik():
         button_kinematik.pack()
 
     def kinematik(self):
-        print "Hello"
+        global find_list, har_list
+        top_beregn = Toplevel(self.top)
+        msg_find = Message(top_beregn, text = "Vælg hvad du vil finde?")
+        msg_find.grid(row = 0)
+        find_list = Listbox(top_beregn, exportselection = 0)
+        find_list.grid(row = 1)
+        muligheder = ["Hastighed: v", "Strækning: s", "Tid: t", "Accelration: a"]
+        for item in muligheder:
+            find_list.insert(END, item)
 
-
+        msg_har = Message(top_beregn, text = "Hvad information har du?")
+        msg_har.grid(row = 0, column = 1)
+        har_list = Listbox(top_beregn, selectmode = MULTIPLE, exportselection = 0)
+        har_list.grid(row = 1, column = 1)
+        for item in muligheder:
+            har_list.insert(END, item)
+        kin_beregning_knap = Button(top_beregn, text = "Test",
+                                    command = self.kin_beregning)
+        kin_beregning_knap.grid(row = 2)
+        
+        
+     
+    def kin_beregning(self):
+        find_valgt = map(int, find_list.curselection())[0]
+        har_valgt = map(int, har_list.curselection())
+        
+        
+        
+        
+    
 
 root = Tk()
 app = MainApp(root)
